36e784e593517ae71967d89435b7c77050098b5f
Joey Kihn <jokihn2002@gmail.com>
2025-02-18 15:34:07 -0500

    Bounds of Input
    Changed input type to number, and placed a min and max value. Additionally, the program runs a function to double check that all values are valid.

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function Optimizer(){
   )
 }
 
-
 function App() {
   const [inputStrength, setStrengthValue] = useState('');
   const [inputDexterity, setDexterityValue] = useState('');
@@ -34,13 +33,23 @@ function App() {
   const wisdomChange = (event) =>{setWisdomValue(event.target.value);};
   const charismaChange = (event) =>{setCharismaValue(event.target.value);};
 
-  const visibleChange = () =>{setVisible(true)};
+  const visibleChange = (val) =>{setVisible(val)};
   const boolStr = false;
 
   const [results, setResultValue] = useState('');
   const resultsChange = (e) => {setResultValue(e);};
 
   let numbs = 0;
+
+  const numbCheck = (stat) => {
+    if(stat > 0 && stat <=20 ){
+      return true
+    }
+    else{
+      return false
+    }
+  };
+
   const shoot = (str, dex, con, int, wis, cha) => {
     numbs = parseInt(str) + parseInt(dex)
     //converting string to INT for calculations
@@ -51,47 +60,54 @@ function App() {
     let WIS = parseInt(wis)
     let CHA = parseInt(cha)
 
-    //compute values for the classes
-    let ARTIFICER = [(INT/10*2+DEX/10*1.5)/2 , "Artificer"]
-    let BARBARIAN = [(STR/10*2+CON/10*1.5)/2, "Barbarian"]
-    let BARD = [(CHA/10*2+DEX/10*1.5)/2, "Bard"]
-    let BLOODHUNTER = [(DEX/10*2+CON/10*1.5)/2, "Blood Hunter"]
-    let CLERIC = [(WIS/10*2+CON/10*1.5)/2, "Cleric"]
-    let DRUID = [(WIS/10*2+CON/10*1.5)/2, "Druid"]
-    let FIGHTER = [(STR/10*2+DEX/10*1.5)/2, "Fighter"]
-    let MONK = [(DEX/10*2+WIS/10*1.5)/2, "Monk"]
-    let PALADIN = [(CHA/10*2+CON/10*1.5)/2, "Paladin"]
-    let RANGER = [(DEX/10*2+WIS/10*1.5)/2, "Ranger"]
-    let ROGUE = [(DEX/10*2+CHA/10*1.5)/2, "Rogue"]
-    let SORCERER = [(CHA/10*2+CON/10*1.5)/2, "Sorcerer"]
-    let WARLOCK = [(CHA/10*2+CON/10*1.5)/2, "Warlock"]
-    let WIZARD = [(INT/10*2+DEX/10*1.5)/2, "Wizard"]
+    if(numbCheck(STR) && numbCheck(DEX) && numbCheck(CON) && numbCheck(INT) && numbCheck(WIS) && numbCheck(CHA)){
+      //compute values for the classes
+      let ARTIFICER = [(INT/10*2+DEX/10*1.5)/2 , "Artificer"]
+      let BARBARIAN = [(STR/10*2+CON/10*1.5)/2, "Barbarian"]
+      let BARD = [(CHA/10*2+DEX/10*1.5)/2, "Bard"]
+      let BLOODHUNTER = [(DEX/10*2+CON/10*1.5)/2, "Blood Hunter"]
+      let CLERIC = [(WIS/10*2+CON/10*1.5)/2, "Cleric"]
+      let DRUID = [(WIS/10*2+CON/10*1.5)/2, "Druid"]
+      let FIGHTER = [(STR/10*2+DEX/10*1.5)/2, "Fighter"]
+      let MONK = [(DEX/10*2+WIS/10*1.5)/2, "Monk"]
+      let PALADIN = [(CHA/10*2+CON/10*1.5)/2, "Paladin"]
+      let RANGER = [(DEX/10*2+WIS/10*1.5)/2, "Ranger"]
+      let ROGUE = [(DEX/10*2+CHA/10*1.5)/2, "Rogue"]
+      let SORCERER = [(CHA/10*2+CON/10*1.5)/2, "Sorcerer"]
+      let WARLOCK = [(CHA/10*2+CON/10*1.5)/2, "Warlock"]
+      let WIZARD = [(INT/10*2+DEX/10*1.5)/2, "Wizard"]
 
-    //add classes to the list if they are not ignored
-    var v = []
-        v.push(ARTIFICER)
-        v.push(BARBARIAN)
-        v.push(BARD)
-        v.push(BLOODHUNTER)
-        v.push(CLERIC)
-        v.push(DRUID)
-        v.push(FIGHTER)
-        v.push(MONK)
-        v.push(PALADIN)
-        v.push(RANGER)
-        v.push(ROGUE)
-        v.push(SORCERER)
-        v.push(WARLOCK)
-        v.push(WIZARD)
+      //add classes to the list if they are not ignored
+      var v = []
+          v.push(ARTIFICER)
+          v.push(BARBARIAN)
+          v.push(BARD)
+          v.push(BLOODHUNTER)
+          v.push(CLERIC)
+          v.push(DRUID)
+          v.push(FIGHTER)
+          v.push(MONK)
+          v.push(PALADIN)
+          v.push(RANGER)
+          v.push(ROGUE)
+          v.push(SORCERER)
+          v.push(WARLOCK)
+          v.push(WIZARD)
 
-    v.sort(function(a,b){return b[0] - a[0]})
+      v.sort(function(a,b){return b[0] - a[0]})
 
-    let output = ""
-    for(var i=0; i<v.length;i++){
-      output += v[i][1] + " with a score of " + v[i][0] + "\n\n"
+      let output = ""
+      for(var i=0; i<v.length;i++){
+        output += v[i][1] + " with a score of " + v[i][0] + "\n\n"
+      }
+      
+      visibleChange(true)
+      resultsChange(output)
     }
-    visibleChange()
-    resultsChange(output)
+    else{
+      visibleChange(true)
+      resultsChange("Only enter values between 0-20")
+    }
   };
   return (
     <>
@@ -137,12 +153,12 @@ function App() {
         {// need to add pattern detection later
         }
   <div>
-    Strength:  <input type= "text" value={inputStrength} onChange={strengthChange} /><br></br>
-    Dexterity:  <input type= "text" value={inputDexterity} onChange={dexterityChange} /><br></br>
-    Constitution:  <input type= "text" value={inputConstitution} onChange={constitutionChange} /><br></br>
-    Intelligence:  <input type= "text" value={inputIntelligence} onChange={intelligenceChange} /><br></br>
-    Wisdom:  <input type= "text" value={inputWisdom} onChange={wisdomChange} /><br></br>
-    Charisma:  <input type= "text" value={inputCharisma} onChange={charismaChange} /><br></br>
+    Strength:  <input type= "number" value={inputStrength} onChange={strengthChange} min="1" max="20"/><br></br>
+    Dexterity:  <input type= "number" value={inputDexterity} onChange={dexterityChange} min="1" max="20"/><br></br>
+    Constitution:  <input type= "number" value={inputConstitution} onChange={constitutionChange} min="1" max="20"/><br></br>
+    Intelligence:  <input type= "number" value={inputIntelligence} onChange={intelligenceChange} min="1" max="20"/><br></br>
+    Wisdom:  <input type= "number" value={inputWisdom} onChange={wisdomChange} min="1" max="20"/><br></br>
+    Charisma:  <input type= "number" value={inputCharisma} onChange={charismaChange} min="1" max="20"/><br></br>
 
     {/* add the check boxes in later
     <input type="checkbox" value={boolStr} onChange={testing}/>*/}
@@ -159,7 +175,7 @@ function App() {
   {/* Outputs results after the button is Clicked */}
   <div>
     {isVisible && <p>
-    The range of scores is from 3.5-0.175. The Highest valued classes are the
+    The range of scores is from 0-3.5. The Highest valued classes are the
     ones we recommend you build!
     <br />
     If you do not like the recommendation, try adjusting where you put stats or
